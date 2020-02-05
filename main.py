224cb1498f04448d13a229122b90b4ef781fb949
Gustavo <gustavo@cloudster.com.br>
2020-02-05 20:08:34 -0300

    codepipeline integration

--- main.py
+++ main.py
@@ -14,21 +14,28 @@ logger.setLevel(LOGLEVEL.upper())
 
 
 def lambda_handler(event, context):
-
-    now = datetime.now()
-    now_timestamp = now.strftime(TIMESTAMP_FORMAT)
-
-    snapshot_identifier = ('{}{}'.format(DB_IDENTIFIER, now_timestamp))
+    codepipeline = boto3.client('codepipeline')
+    job_id = event['CodePipeline.job']['id']
 
     try:
-        response = RDS.create_db_snapshot(
+        now = datetime.now()
+        now_timestamp = now.strftime(TIMESTAMP_FORMAT)
+
+        snapshot_identifier = ('{}-{}'.format(DB_IDENTIFIER, now_timestamp))
+        snapshot = RDS.create_db_snapshot(
             DBSnapshotIdentifier=snapshot_identifier,
             DBInstanceIdentifier=DB_IDENTIFIER,
+            ) 
+        logger.debug(snapshot)
+        response = codepipeline.put_job_success_result(jobId=job_id)
+
+    except Exception as error:
+        logger.info('Could not create snapshot %s (%s)' % (snapshot_identifier, error))
+        response = codepipeline.put_job_failure_result(
+            jobId=job_id,
+            failureDetails={'type': 'JobFailed', 'message': error}
             )
-
-    except Exception as e:
-        logger.info('Could not create snapshot %s (%s)' % (snapshot_identifier, e))
-
+    logger.debug(response)
 
 if __name__ == '__main__':
     lambda_handler(None, None)
